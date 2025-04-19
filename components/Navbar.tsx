ca1f16d14226a27e7fd70ebfaa088c844a5b895c
shrudh14007 <shrudhir007@gmail.com>
2025-04-19 23:58:21 +0100

    added changes to navbar

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,18 +1,22 @@
 "use client";
-import React from 'react'
-import Logo from './Logo'
+import React, { useState } from 'react'
+import Logo, { LogoMobile } from './Logo'
 import { usePathname } from 'next/navigation';
 import Link from 'next/link';
 import { cn } from '@/lib/utils';
-import { buttonVariants } from './ui/button';
-import { Ghost } from 'lucide-react';
+import { Button, buttonVariants } from './ui/button';
+import { Ghost, Menu } from 'lucide-react';
 import { start } from 'repl';
 import { UserButton } from '@clerk/nextjs';
+import { ThemeSwitcherBtn } from './ThemeSwitcherBtn';
+import { Sheet, SheetContent, SheetTrigger } from './ui/sheet';
+import { Share_Tech } from 'next/font/google';
 
 function Navbar() {
   return (
     <>
     <DesktopNavbar /> 
+    <MobileNavbar />
     </>
   )
 }
@@ -23,6 +27,43 @@ const items = [
   {label : "Manage", link: "/manage"}
   
 ];
+
+function MobileNavbar(){
+  const [isOpen , setIsOpen] = useState(false);
+
+  return (<div className=" block border-separate bg-background md:hidden">
+    <nav className=" container flex items-center justify-between px-8"> 
+      <Sheet open ={isOpen} onOpenChange={setIsOpen}>
+        <SheetTrigger asChild>
+          <Button variant={"ghost"} size={"icon"}>
+            <Menu />
+          </Button>
+        </SheetTrigger>
+        <SheetContent className="w-[400px] sm:w-[540px]"
+        side="left">
+          <Logo />
+          <div className="flex flex-col gap-1 pt-4">
+            {items.map((item) =>(
+              <NavbarItem
+                key={items.label}
+                link={item.link}
+                label={items.label}
+                clickCallback = {() => setIsOpen((prev) => !prev)}
+              /> 
+              ))}
+          </div>
+        </SheetContent>
+      </Sheet>
+      <div className="flex h-[80px] min-h-[60px] items-center gap-x-4">
+        <LogoMobile />
+      </div>
+      <div className="flex items-center gap-2">
+      <ThemeSwitcherBtn />
+      </div>
+    </nav>
+  </div>
+  );
+}
 
 function DesktopNavbar() {
     return (
@@ -43,16 +84,18 @@ function DesktopNavbar() {
                   </div>
                 </div>
                 <div className="flex items-center gap-2">
-                  <ThemeSwitcherBtn />
-                  <UserButton afterSignOutUrl="/sign-in" /> 
+                  <ThemeSwitcherBtn/>
+                  <UserButton /> 
                 </div>
             </nav>
         </div>
     )
 }
-function NavbarItem({link,label}: {
+function NavbarItem({link,label, onClick: clickCallback}: {
     link:string;
-    label:string
+    label:string,
+    clickCallback? : () => void;
+
 
 }) {
     const pathname= usePathname();
@@ -65,7 +108,12 @@ function NavbarItem({link,label}: {
           buttonVariants({variant : "ghost" }),
          "w-full justify-start text-lg text-muted-foreground",
          isActive && "text-foreground"
-        )}>
+        )}
+        onClick={() =>{
+          if (clickCallback) clickCallback();
+        } }
+        >
+
           {label}
           </Link>
           {
