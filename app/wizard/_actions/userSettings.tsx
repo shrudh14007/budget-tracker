a74765d88b1e0617f2ecef33346a8f23835d085f
shrudh14007 <shrudhir007@gmail.com>
2025-04-22 23:56:37 +0100

    created user setting for currency change

---
+++ app/wizard/_actions/userSettings.tsx
@@ -0,0 +1,35 @@
+"use server ";
+
+import { prisma } from "@/lib/prisma";
+import { UpdateUserCurrencySchema } from "@/schema/userSettings";
+import { currentUser } from "@clerk/nextjs/server";
+import { UserSearch } from "lucide-react";
+import { redirect } from "next/navigation";
+
+export async function UpdateUserCurrency(currency:string) {
+    const parsedBody = UpdateUserCurrencySchema.safeParse(
+        {currency},
+    );
+
+    if(!parsedBody.success){
+        throw parsedBody.error;
+
+    }
+    
+    const user = await currentUser();
+    if(!user) {
+        redirect("sign-in");
+
+    }
+    const UserSettings = await prisma.userSettings.update({
+        where:{
+            userId: user.id,
+        },
+        data: {
+            currency,
+
+        },
+    });
+
+    return UserSettings;
+}
