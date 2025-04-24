56b5eafd7bc8fe8916c850da16f889bb062c0d26
shrudh14007 <shrudhir007@gmail.com>
2025-04-24 23:59:05 +0100

    changes to dahboard page

--- app/(dashboard)/page.tsx
+++ app/(dashboard)/page.tsx
@@ -1,9 +1,33 @@
+import { prisma } from '@/lib/prisma';
+import { currentUser } from '@clerk/nextjs/server'
+import { redirect } from 'next/navigation';
 import React from 'react'
 
-function page() {
-  return (
-    <div>page</div>
-  )
+async function page() {
+  const user = await currentUser();
+  if(!user){
+    redirect ("/sign-in");
+
+  }
+
+  const userSettings = await prisma.userSettings.findUnique
+  ({
+      where:{
+        userId : user.id,
+
+      }
+  });
+
+  if(!userSettings){
+    redirect("/wizard");
+  }
+  return <div className=" h-full bg-background">
+    <div className="border-b bg-card">
+      <div className="container flex flex-wrap items-center justify-between gap-6 py-8">
+        <p className="text-3x1 font-bold">Hello,{user.firstName}! 👋</p>
+      </div>
+    </div>
+  </div>
 }
 
 export default page
