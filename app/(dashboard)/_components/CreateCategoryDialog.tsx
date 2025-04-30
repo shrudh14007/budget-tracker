458f5dbcb7d9d0ace41dce6232efe32af1d69763
shrudh14007 <shrudhir007@gmail.com>
2025-05-01 00:33:40 +0100

    modified category picker dialog

--- app/(dashboard)/_components/CreateCategoryDialog.tsx
+++ app/(dashboard)/_components/CreateCategoryDialog.tsx
@@ -4,13 +4,17 @@ import { Button } from '@/components/ui/button';
 import { Dialog, DialogContent, DialogDescription, DialogHeader, DialogTitle, DialogTrigger } from '@/components/ui/dialog';
 import { Form, FormControl, FormDescription, FormField, FormItem, FormLabel } from '@/components/ui/form';
 import { Input } from '@/components/ui/input';
+import { Popover, PopoverTrigger } from '@/components/ui/popover';
 import { TransactionType } from '@/lib/types';
 import { cn } from '@/lib/utils';
 import { CreateCategorySchema, CreateCategorySchemaType } from '@/schema/categories';
 import { zodResolver } from '@hookform/resolvers/zod';
-import { PlusSquare } from 'lucide-react';
+import { PopoverContent } from '@radix-ui/react-popover';
+import { CircleOff, PlusSquare } from 'lucide-react';
 import React, { useState } from 'react'
 import { useForm } from 'react-hook-form';
+import Picker from "@emoji-mart/react";
+import data from "@emoji-mart/data";
 interface Props{
     type : TransactionType;
 }
@@ -52,7 +56,7 @@ function CreateCategoryDialog({type} : Props) {
             </DialogHeader>
             <Form {...form}>
                 <form className='space-y-8'>
-                <FormField 
+        <FormField 
           control={form.control}
           name = "name"
           render={({ field }) => (
@@ -68,8 +72,54 @@ function CreateCategoryDialog({type} : Props) {
             </FormItem>
 
           )}
-          />
-                </form>
+            />
+            
+            <FormField 
+          control={form.control}
+          name = "icon"
+          render={({ field }) => (
+            <FormItem> 
+              <FormLabel>Icon</FormLabel>
+              <FormControl>
+                <Popover>
+                    <PopoverTrigger asChild>
+                        <Button variant={"outline"}
+                        className='h-[100px] w-full'>
+                            {form.watch("icon") ? (
+                                <div className='flex flex-col items-center gap-2'> 
+                                <span className="text-5x1" role='img'>
+                                    {field.value}
+                                </span>
+                                <p className='text-xs text-muted-foreground'>
+                                    Click to change
+                                </p>
+                                </div>
+                            ): (
+                                <div className='flex flex-col items-center gap-2'> 
+                                <CircleOff className='h-[48px] w-[48px]' /> 
+                                <p className='text-xs text-muted-foreground'>
+                                    Click to Select
+                                </p>
+                                </div>
+                            )}
+                        </Button>
+
+                    </PopoverTrigger>
+                    <PopoverContent className='w-full'>
+                        <Picker data = {data} /> 
+                    </PopoverContent>
+                </Popover>
+                 
+              </FormControl>
+              <FormDescription>
+                This is how your category will appear in the app.
+              </FormDescription>
+            </FormItem>
+
+          )}
+            />
+
+            </form>
             </Form>
         </DialogContent>
 
