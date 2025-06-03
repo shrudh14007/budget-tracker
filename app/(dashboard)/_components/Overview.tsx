4e691d82e5d2ecbb954c6c7489d9f57e27c0bd61
shrudh14007 <shrudhir007@gmail.com>
2025-06-03 01:54:08 +0100

    Added an overview section

---
+++ app/(dashboard)/_components/Overview.tsx
@@ -0,0 +1,42 @@
+"use client";
+import { DateInput, DateRangePicker } from '@/components/ui/date-range-picker';
+import { MAX_DATE_RANGE_DAYS } from '@/lib/constants';
+import { UserSettings } from '@/lib/generated/prisma';
+import { differenceInDays, startOfMonth } from 'date-fns';
+import React, { useState } from 'react'
+import { toast } from 'sonner';
+
+function Overview({userSettings}: {userSettings : UserSettings}) {
+    const[dateRange, setDateRange] = useState<{from: Date ; to:Date}>({
+        from: startOfMonth(new Date()),
+        to: new Date(), 
+    })
+  return (
+    <div className="container-flex flex-wrap items-end justify-between gap-2 py-6">
+        <h2 className='text-3xl font-bold'>Overview</h2>
+        <div className='flex items-center gap-3'>
+            <DateRangePicker
+            initialDateFrom={dateRange.from}
+            initialCompareTo={dateRange.to}
+            showCompare={values=> {
+                const {from,to} = values.range;
+                //update the date only if both the dates are set
+
+                if(!from || !to) return;
+                if(differenceInDays(to,from ) > MAX_DATE_RANGE_DAYS){
+                    toast.error("The selected date range is too big.Max allowed date range is ${MAX_DATE_RANGE_DAYS} days! "
+
+                    );
+                    return;
+
+                }
+                setDateRange({from,to});
+
+            }}
+            />
+        </div>
+    </div>
+  )
+}
+
+export default Overview
