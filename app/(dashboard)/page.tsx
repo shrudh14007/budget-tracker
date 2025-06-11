5e264cb61ecfd2c78a80e87dbed886af9759b399
shrudh14007 <shrudhir007@gmail.com>
2025-06-11 23:33:50 +0100

    added  month in history selector

--- app/(dashboard)/page.tsx
+++ app/(dashboard)/page.tsx
@@ -5,6 +5,9 @@ import { redirect } from 'next/navigation';
 import React from 'react'
 import CreateTransactionDialog from './_components/CreateTransactionDialog';
 import Overview from './_components/Overview';
+import History from './_components/History';
+
+
 
 async function page() {
   const user = await currentUser();
@@ -50,7 +53,8 @@ async function page() {
       </div>
     </div>
     <Overview userSettings={userSettings}/>
+    <History userSettings={userSettings}/>
   </div>
-}
+} 
 
 export default page
