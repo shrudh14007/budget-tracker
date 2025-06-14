2dd3113b6a2b40244f1c80733b8f698899e23129
shrudh14007 <shrudhir007@gmail.com>
2025-06-14 02:08:03 +0100

    completed the transaction page

--- app/api/transactions-history/route.ts
+++ app/api/transactions-history/route.ts
@@ -1,3 +1,5 @@
+import { GetFormatterForCurrency } from "@/lib/helpers";
+import { prisma } from "@/lib/prisma";
 import { OverviewQuerySchema } from "@/schema/overview";
 import { currentUser } from "@clerk/nextjs/server";
 import { redirect } from "next/navigation";
@@ -5,7 +7,7 @@ import { redirect } from "next/navigation";
 export async function GET(request:Request) {
     const user = await currentUser();
     if(!user){
-        redirect(/sign-in);
+        redirect("/sign-in");
 
     }
     const {searchParams} = new URL(request.url);
@@ -30,7 +32,40 @@ export async function GET(request:Request) {
         queryParams.data.to
     );
 
-    return transactions;
-    
-    
+    return Response.json(transactions);
+}
+
+export type GetTransactionsHistoryResponseType = Awaited<ReturnType<typeof getTransactionsHistory>>;
+async function getTransactionsHistory(userId : string , from : Date,to: Date){
+    const userSettings = await prisma.userSettings.findUnique({
+        where:{
+            userId,
+
+        }
+    });
+    if(!userSettings){
+        throw new Error("user settings not found");
+
+    }
+    const formatter = GetFormatterForCurrency(userSettings.currency);
+
+
+    const transactions = await prisma.transaction.findMany({
+        where:{
+            userId,
+            date:{
+                gte:from,
+                lte:to,
+
+            },
+
+        },
+        orderBy:{
+            date:"desc",
+        },
+    });
+    return transactions.map(transaction => ({
+        ...transaction,
+        formattedAmount: formatter.format(transaction.amount),  
+    }) )
 }
